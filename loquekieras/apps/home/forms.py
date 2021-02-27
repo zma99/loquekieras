b95b9d771a7a4028283a4ad93ccfcf17b79e4759
Alan Zanazzo <alanzanazzo44@gmail.com>
2021-02-27 18:47:24 -0300

    nuevos templates

---
+++ loquekieras/apps/home/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+## from .models import Usuario
+
+class userForm(forms.ModelForm):
+    '''
+        class Meta:
+        model = Usuario
+        fields = '__all__'
+    '''
+    pass
