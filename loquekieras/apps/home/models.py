15fa9a51750ae039b4fb574dcee547b7e22751f6
claudio <cmarangoni8@gmail.com>
2021-02-27 17:56:43 -0300

    se modifico tabla producto

--- loquekieras/apps/home/models.py
+++ loquekieras/apps/home/models.py
@@ -3,7 +3,6 @@ from django.db import models
 
 class Producto(models.Model):
     titulo = models.CharField(max_length=100)
-    nombre= models.CharField(max_length=10)
     marca = models.CharField(max_length=15)
     modelo = models.CharField(max_length=20)
     descripcion = models.CharField(max_length=50)
