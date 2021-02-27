961f35262967f8bffb5fc1c661d39b414366e2d1
claudio <cmarangoni8@gmail.com>
2021-02-27 17:51:04 -0300

    registro app y tabla producto

--- loquekieras/apps/home/models.py
+++ loquekieras/apps/home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Producto(models.Model):
+    titulo = models.CharField(max_length=100)
+    nombre= models.CharField(max_length=10)
+    marca = models.CharField(max_length=15)
+    modelo = models.CharField(max_length=20)
+    descripcion = models.CharField(max_length=50)
+
+    
+
