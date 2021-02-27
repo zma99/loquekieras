c27ad04429b3fc318a084da3eef0ac596e56884e
claudio <cmarangoni8@gmail.com>
2021-02-27 18:18:54 -0300

    agrego id a tabla producto y se creo tabla usuario

--- loquekieras/apps/home/models.py
+++ loquekieras/apps/home/models.py
@@ -6,6 +6,17 @@ class Producto(models.Model):
     marca = models.CharField(max_length=15)
     modelo = models.CharField(max_length=20)
     descripcion = models.CharField(max_length=50)
+    id = models.AutoField(primary_key = True)
 
     
+
+class Usuario(models.Model):
+    usuario = models.CharField(max_length=15, primary_key = True, unique = True) 
+    email = models.EmailField(max_length=20)
+    nombre = models.CharField(max_length=10)
+    apellido = models.CharField(max_length=15)
+    edad = models.IntegerField(max_length=3)
+    direccion = models.CharField(max_length=50)
+    ciudad = models.CharField(max_length=15)
+    provincia = models.CharField(max_length=15)
 
